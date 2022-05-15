819e8ca29f85926b1371fa3ccce5428f44edf83c
cbjoon <qudwns017@naver.com>
2022-05-15 19:02:23 +0900

    first

--- client/src/components/views/api/kakao/MapContainer.js
+++ client/src/components/views/api/kakao/MapContainer.js
@@ -4,7 +4,7 @@ export default function MapContainer() {
   const container = document.getElementById("myMap");
   const options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
+    level: 4,
   };
   const map = new kakao.maps.Map(container, options);
 }
